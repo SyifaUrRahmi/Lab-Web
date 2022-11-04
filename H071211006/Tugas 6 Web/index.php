540a8b804145ec399aee248c9c02cd6868d0a981
hikma12 <hikmanurrr@gmail.com>
2022-11-05 03:01:38 +0800

    TUgas 6

--- H071211006/Tugas 6 Web/index.php
+++ H071211006/Tugas 6 Web/index.php
@@ -91,7 +91,7 @@ if (isset($_POST['simpan'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <style>
         body {
-            background: black;
+            background-color: black;
             font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
         }
 
