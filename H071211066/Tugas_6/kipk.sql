2db55a1fe0cddee778dbeb0480b145d41c09b8ec
SyifaUrRahmi <sifasisi@gmail.com>
2022-11-05 12:07:03 +0800

    Edit file kipk.sql

--- H071211066/Tugas_6/kipk.sql
+++ H071211066/Tugas_6/kipk.sql
@@ -1,13 +1,73 @@
-CREATE DATABASE kipk;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 05, 2022 at 05:02 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
-USE kipk;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE penerima (
-id INT(11) AUTO_INCREMENT,
-nim VARCHAR(25) UNIQUE,
-nama VARCHAR(100),
-no_kipk VARCHAR(25) UNIQUE,
-fakultas VARCHAR(25),
-prodi VARCHAR(100),
-angkatan VARCHAR(25)
-);
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kipk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penerima`
+--
+
+CREATE TABLE `penerima` (
+  `id` int(11) NOT NULL,
+  `nim` varchar(25) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `no_kipk` varchar(25) NOT NULL,
+  `fakultas` varchar(25) NOT NULL,
+  `prodi` varchar(100) NOT NULL,
+  `angkatan` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `penerima`
+--
+
+INSERT INTO `penerima` (`id`, `nim`, `nama`, `no_kipk`, `fakultas`, `prodi`, `angkatan`) VALUES
+(3, 'H071211066', 'Syifa Ur Rahmi', 'BY82W3K', 'FMIPA', 'Sistem Informasi', '2021'),
+(4, 'H051211030', 'Syifa Ur Rahmah', 'PR90W3L', 'FMIPA', 'Statistika', '2021');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `penerima`
+--
+ALTER TABLE `penerima`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nim` (`nim`),
+  ADD UNIQUE KEY `no_kipk` (`no_kipk`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `penerima`
+--
+ALTER TABLE `penerima`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
