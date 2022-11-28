c93239163445434e0da99139d07de98de96b91af
SyifaUrRahmi <sifasisi@gmail.com>
2022-11-28 15:56:35 +0800

    Laravel 2 part 1

---
+++ H071211066/Tugas_9/laravel-2/resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<!-- Mengambil Layout utama -->
+@extends('layouts.main')
+
+<!-- Berisi hal-hal berbeda dari setiap halaman yang akan di tampilkan di layout utama -->
+@section('container')
+<h1>Halaman {{ $title }}</h1>
+<h3>{{ $nama }}</h3>
+  <p>{{ $email }}</p>
+  <img src="assets/images/{{ $img }}" alt="{{ $nama }}" width="200" class="img-thumbnail rounded-circle">
+@endsection
