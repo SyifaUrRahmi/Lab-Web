172619cc3cc5cb1ca825f0e7faef7b36d480750c
hikma12 <hikmahnurrrrr@gmail.com>
2022-12-02 08:03:02 +0800

    Tugas 8

---
+++ H071211006/tugas8/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        // User::create([
+        //     'name' => 'Hikma',
+        //     'username' => 'lightma',
+        //     'email' => 'hikmanurrrrr@gmail.com',
+        //     'password' => bcrypt('password')
+        // ]); 
+
+        User::factory(5)->create();  
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design'
+        ]);
+
+        Post::factory(25)->create();        
+       
+    }
+}
