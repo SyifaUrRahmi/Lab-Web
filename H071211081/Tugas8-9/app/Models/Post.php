5be7572ba16447e3257d8314a05539f1c9897c67
muslih2018 <sahmatmuslih@gmail.com>
2022-12-09 02:56:53 +0700

    kumpul tugas 8-9 sandika galih

---
+++ H071211081/Tugas8-9/app/Models/Post.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
+
+    protected $with = ['category', 'author'];
+
+    // query scope
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            return $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%');
+        });
+
+        $query->when($filters['category'] ?? false, function ($query, $category) {
+            return $query->whereHas('category', function ($query) use ($category) {
+                $query->where('slug', $category);
+            });
+        });
+
+        $query->when(
+            $filters['author'] ?? false,
+            fn ($query, $author) =>
+            $query->whereHas(
+                'author',
+                fn ($query) =>
+                $query->where('username', $author)
+            )
+        );
+    }
+
+    public function category()
+    {
+        // Relasi
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    // setiap route mencari slug
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+}
