ee9b34d873a8e391ecac60b21d946a8593ffb1ea
PutriAuliaNur <ptrslsbl1215@gmail.com>
2022-12-15 23:10:12 +0800

    Kumpul Tugas 8 dan 9

---
+++ H071211046/Materi_08 dan 09/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        // User::create([
+        //     'name' => 'Putri',
+        //     'username' => 'Shasa',
+        //     'email' => 'ptraulia1215@gmail.com',
+        //     'password' => ('password')
+        // ]); 
+
+        User::factory(5)->create();  
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design'
+        ]);
+
+        Post::factory(25)->create();        
+       
+    }
+}
