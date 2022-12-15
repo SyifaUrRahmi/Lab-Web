f1eddf829fc10756086bb6ea575f663fe62121c5
hikmaa <106948962+hikma12@users.noreply.github.com>
2022-12-15 16:41:20 +0800

    tugas akhir

---
+++ H071211006/tugas_akhir/database/migrations/2021_01_27_094756_create_post_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->string('sampul', 50);
+            $table->string('judul');
+            $table->text('konten');
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
